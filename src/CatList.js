63d6cd2851b8d7a431b48ac93a355ae8f1830a3e
ts-miller <tylerstephen11@gmail.com>
2021-01-10 15:37:24 -0500

    commit tree 7e435f7e16b7c1c5a96ddb363ac86e77b5254720

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,14 @@
-// write your CatList component here
+import React, { Component } from 'react'
+
+class CatList extends Component {
+  
+    render() {
+      return (
+        <div>
+          {this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)}
+        </div>
+      )
+    }
+  }
+
+export default CatList
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import catsReducer from './reducers/catsReducer'
+import thunk from 'redux-thunk'
 
+const store = createStore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
